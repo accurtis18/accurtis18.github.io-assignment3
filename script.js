e0859c40c29f665e6217a9e446b3d0ccd7e22936
Alex <achriscurtis@gmail.com>
2020-04-17 23:19:16 -0500

    get and check length scripts

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+// // Assignment Code
+// var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -10,5 +10,35 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
+
+// fucntion checkLength(pLength) {
+//     if(pLength > 7 && pLength < 129){
+//         return true;
+//     } else{
+//         return false;
+//     }
+// }
+
+function checkLength(pLength){
+        if(pLength > 7 && pLength < 129){
+            return true;
+        } else{
+            return false;
+    }
+}
+
+function getLen() {
+    var lengthPass = false
+    while (lengthPass !== true){
+        var passLength = prompt("How long should your password be? (between 8 and 128 characters)");
+        lengthPass = checkLength(passLength);
+    }
+    var passwordText = document.querySelector('#password');
+    passwordText.value = passLength;
+
+}
+
+var generateBtn = document.querySelector("#generate");
+generateBtn.addEventListener("click", getLen);
