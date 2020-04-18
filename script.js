527b1590d0384920940494184b123986606f1eb3
Alex <achriscurtis@gmail.com>
2020-04-17 23:32:40 -0500

    create requirement gather function

--- script.js
+++ script.js
@@ -2,13 +2,13 @@
 // var generateBtn = document.querySelector("#generate");
 
 // // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+// function writePassword() {
+//   var password = generatePassword();
+//   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+//   passwordText.value = password;
 
-}
+// }
 
 // // Add event listener to generate button
 // generateBtn.addEventListener("click", writePassword);
@@ -20,6 +20,19 @@ function writePassword() {
 //         return false;
 //     }
 // }
+
+function getRequirements(){
+    var reqs = []
+    reqs.push(confirm("Should it contain lower case letters?"));
+    reqs.push(confirm("Should it contain upper case letters?"));
+    reqs.push(confirm("Should it contain numbers?"));
+    reqs.push(confirm("Should it contain at least one of these special characters? (!, *, $, #, -)"));
+
+    var passwordText = document.querySelector('#password');
+    passwordText.value = reqs;
+    return reqs;
+}
+
 
 function checkLength(pLength){
         if(pLength > 7 && pLength < 129){
@@ -35,10 +48,12 @@ function getLen() {
         var passLength = prompt("How long should your password be? (between 8 and 128 characters)");
         lengthPass = checkLength(passLength);
     }
-    var passwordText = document.querySelector('#password');
-    passwordText.value = passLength;
+}
 
+function createPassword(){
+    getLen();
+    var requirements = getRequirements();
 }
 
 var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", getLen);
+generateBtn.addEventListener("click", createPassword);
